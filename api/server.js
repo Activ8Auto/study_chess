7808980f6a14133d162760ef4b05fdd08ab05c6c
Activ8Auto <Taylor@activ8automation.com>
2025-03-26 23:58:01 -0600

    fuck

--- api/server.js
+++ api/server.js
@@ -2,3 +2,9 @@ const serverless = require("serverless-http");
 const app = require("./index");
 
 module.exports.handler = serverless(app);
+
+// Local test
+if (process.env.NODE_ENV !== "production") {
+  const port = 5001;
+  app.listen(port, () => console.log(`Running locally on port ${port}`));
+}
