3eb68761b4ec7d03c1a424d2b142daff6edd78c2
Activ8Auto <Taylor@activ8automation.com>
2025-03-27 10:11:50 -0600

    fuck

--- api/server.js
+++ api/server.js
@@ -1,18 +1,6 @@
-// const serverless = require("serverless-http");
-// const app = require("./index");
-
-
-
-// module.exports.handler = serverless(app);
-
-// // // Local test
-// // if (process.env.NODE_ENV !== "production") {
-// //   const port = 5001;
-// //   app.listen(port, () => console.log(`Running locally on port ${port}`));
-// // }
-
 // api/server.js
-module.exports.handler = (req, res) => {
-    console.log("Handler invoked");
-    res.status(200).json({ message: "Test handler works" });
-  };
+const serverless = require("serverless-http");
+const app = require("./index"); // Your Express app
+
+// Export the handler explicitly
+module.exports.handler = serverless(app);
