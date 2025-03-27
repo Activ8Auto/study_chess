5f52824bcae994a433fc6c76426c53d849f56078
Activ8Auto <Taylor@activ8automation.com>
2025-03-27 00:10:57 -0600

    fuck

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 const serverless = require("serverless-http");
 const app = require("./index");
 
-module.exports.handler = (req, res) => {
-    res.status(200).json({ message: "Test handler works" });
-  };
 
-// module.exports.handler = serverless(app);
+
+module.exports.handler = serverless(app);
 
 // // Local test
 // if (process.env.NODE_ENV !== "production") {
