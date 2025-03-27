f60a36c3cd9bcc46cd8c3239e6ef6f9d5ee3285a
Activ8Auto <Taylor@activ8automation.com>
2025-03-27 00:13:14 -0600

    Add log to test handler

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
-const serverless = require("serverless-http");
-const app = require("./index");
+// const serverless = require("serverless-http");
+// const app = require("./index");
 
 
 
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
 
-// // Local test
-// if (process.env.NODE_ENV !== "production") {
-//   const port = 5001;
-//   app.listen(port, () => console.log(`Running locally on port ${port}`));
-// }
+// // // Local test
+// // if (process.env.NODE_ENV !== "production") {
+// //   const port = 5001;
+// //   app.listen(port, () => console.log(`Running locally on port ${port}`));
+// // }
+
+// api/server.js
+module.exports.handler = (req, res) => {
+    console.log("Handler invoked");
+    res.status(200).json({ message: "Test handler works" });
+  };
